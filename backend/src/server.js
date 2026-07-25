3be7b9cd8ba8e73aabf777fa62cef0c959267917
Kanhaiya Lal Gurjar <114682493+kanhaiya2516@users.noreply.github.com>
2026-07-25 14:12:54 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ import { app, server } from "./lib/socket.js";
 
 const __dirname = path.resolve();
 
-const PORT = ENV.PORT || 3000;
+const PORT = ENV.PORT || 5000;
 
 app.use(express.json({ limit: "5mb" })); // req.body
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
